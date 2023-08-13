e2ea762f4caf36e44205a7f93536602071c44963
joaquimchianca <joaquimchianca@gmail.com>
2023-08-13 01:53:16 -0300

    feat: bean validation

--- src/main/kotlin/br/joaquim/forum/model/topic/dto/TopicInput.kt
+++ src/main/kotlin/br/joaquim/forum/model/topic/dto/TopicInput.kt
@@ -1,8 +1,28 @@
 package br.joaquim.forum.model.topic.dto
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
 data class TopicInput(
+    @field:NotNull(message = "title must not be null")
+    @field:NotBlank(message = "title must not be blank")
+    @field:Min(value = 3, message = "title is too short")
+    @field:Max(value = 50, message = "title is too long")
     val title: String,
+
+    @field:NotNull(message = "body text must not be null")
+    @field:NotBlank(message = "body text must not be blank")
+    @field:Min(value = 3, message = "body text is too short")
     val text: String,
+
+    @field:NotNull(message = "author must not be null")
+    @field:NotBlank(message = "author must have a name")
+    @field:Min(value = 3, message = "author name is too short")
     val author: String,
+
+    @field:NotNull(message = "category must not be null")
+    @field:NotBlank(message = "category must have a name")
     val category: String
 )
--- src/main/kotlin/br/joaquim/forum/model/topic/dto/TopicUpdate.kt
+++ src/main/kotlin/br/joaquim/forum/model/topic/dto/TopicUpdate.kt
@@ -1,8 +1,19 @@
 package br.joaquim.forum.model.topic.dto
 
-import br.joaquim.forum.model.topic.Topic
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 data class TopicUpdate(
+    @field:NotNull(message = "title must not be null")
+    @field:NotBlank(message = "title must not be blank")
+    @field:Min(value = 3, message = "title is too short")
+    @field:Max(value = 50, message = "title is too long")
     val title: String,
+
+    @field:NotNull(message = "body text must not be null")
+    @field:NotBlank(message = "body text must not be blank")
+    @field:Min(value = 3, message = "body text is too short")
     val text: String
 )
