c7a6d95f26e30277a6473476963c84e534b55fcc
joaquimchianca <joaquimchianca@gmail.com>
2023-08-13 01:54:24 -0300

    fix: removed @OneToMany

--- src/main/kotlin/br/joaquim/forum/model/topic/Topic.kt
+++ src/main/kotlin/br/joaquim/forum/model/topic/Topic.kt
@@ -1,5 +1,7 @@
 package br.joaquim.forum.model.topic
 
+import br.joaquim.forum.model.topic.dto.TopicInput
+import br.joaquim.forum.model.topic.dto.TopicUpdate
 import jakarta.persistence.*
 import org.hibernate.annotations.CreationTimestamp
 import java.time.LocalDateTime
@@ -8,20 +10,30 @@ import java.time.LocalDateTime
 @Table(name = "tb_topic")
 data class Topic(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long,
-    val title: String,
-    val text: String,
-    val author: String,
-    val category: String,
+    @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    private var title: String = "",
+    @Column(columnDefinition = "TEXT")
+    private var text: String = "",
+    private var author: String = "",
+    private var category: String = "",
     @Enumerated(EnumType.STRING)
-    val status: StatusTopic
-) {
+    private var status: StatusTopic = StatusTopic.OPEN,
     @CreationTimestamp
-    val dateCreated: LocalDateTime
-        get() = Local
-    @OneToMany
-    val answers: List<String>
-        get() {
-            TODO()
-        }
+    private val dateCreated: LocalDateTime = LocalDateTime.now(),
+    private var answers: List<String> = listOf()
+) {
+    fun convertWhenCreate(dto: TopicInput): Topic {
+        this.title = dto.title
+        this.text = dto.text
+        this.author = dto.author
+        this.category = dto.category
+        this.answers = listOf()
+        this.status = StatusTopic.OPEN
+        return this
+    }
+
+    fun convertUpdate(dto: TopicUpdate) {
+        this.title = dto.title
+        this.text = dto.text
+    }
 }
